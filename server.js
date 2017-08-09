4b5b86429a4c040a6a78ff3a77b59724d3179124
Katie McBride <archmcbride@hotmail.com>
2017-08-09 11:15:24 -0600

    fixed names in url

--- server.js
+++ server.js
@@ -3,19 +3,30 @@ var express = require('express')
 var app = express()
 
 
-
-app.get('/index2.html', function(req,res){
-	res.sendFile('./index2.html', {root: './public'})
-})
 app.use(express.static('./public'))
 //this checks the files in the public file before moving on to the other get requests
 //this replaces the folowing three get requests.
 //
 //
-// app.get('/', function(req,res){
-// 	res.sendFile('./index.html', {root: './public'})
-// })
+app.get('/denver', function(req,res){
+	res.sendFile('./index.html', {root: './public'})
+})
 
+app.get('/limon_colorado', function(req,res){
+	res.sendFile('./index2.html', {root: './public'})
+})
+
+app.get('/colby_kansas', function(req,res){
+	res.sendFile('./index3.html', {root: './public'})
+})
+
+app.get('/hays_kansas', function(req,res){
+	res.sendFile('./index4.html', {root: './public'})
+})
+
+app.get('/topeka_kansas', function(req,res){
+	res.sendFile('./index5.html', {root: './public'})
+})
 // // app.get('/main.css', function(req,res){
 // 	res.sendFile('./main.css', {root: './public'})
 // })
